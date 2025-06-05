a16da62bbb3be821fcd6b4134186e4e6ce7dc449
Aninha <ana.conceicao.santos@escola.pr.gov.br>
2025-06-05 08:01:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Química',
+    'O que é H²0?',
+    'H²0 é água'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 24 x 32',
+    'É 768'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
